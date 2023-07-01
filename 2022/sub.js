d5d3bead646cbdac3318e3a8e1a328306666a278
caixukunwen <84445083+caixukunwen@users.noreply.github.com>
2023-07-01 10:34:29 +0800

    Update sub.js

--- 2022/sub.js
+++ 2022/sub.js
@@ -1,13 +1,16 @@
-function operator(proxies, targetPlatform) {
+async function operator(proxies = []) {
     const _ = lodash
-    const host = 'tms.dingtalk.com'
-    return proxies.map(p => {
-      const type = _.get(p, 'type')
-      if ('trojan' === type) {
-        if (host) {
-          _.set(p, 'sni', host)
-        }
+    return proxies.map((p = {}) => {
+      if(_.get(p, 'type') === 'trojan') {
+        const name = _.get(p, 'name') || '' // 演示一下 可以用 lodash
+
+        _.set(p, 'name', name + '-后缀') // 名称 添加后缀 怕小白复制出问题 不使用反引号了
+        _.set(p, 'skip-cert-verify', true)  // 改跳过证书验证
+        _.set(p, 'sni', 'v9-dy.ixigua.com') // 改混淆
+
+        // _.set(p, 'udp', true) // 开 udp 一般不用在脚本里改 可以界面上开
+        // _.set(p, 'tfo', true) // 开 tfo 一般不用在脚本里改 可以界面上开
       }
       return p
     })
-  }
+}
